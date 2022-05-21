396935a8427b2c74247ff7bbd57fe429b55e8d57
Daisy Fang <nffa@student.unimelb.edu.au>
2022-05-21 17:39:54 +1000

    reupdate

---
+++ homepage 1/script.js
@@ -0,0 +1,45 @@
+var jd1 = document.getElementsByClassName('sousuo')[0]
+console.log(jd1.children[0])
+jd1.children[1].addEventListener('click', function () {
+
+    jd1.children[1].style.display = 'none'
+    jd1.children[2].style.display = 'flex'
+    
+    jd1.children[2].addEventListener('click', function () {
+        window.location.href="../product listing 2/index.html";
+
+    }, false)
+}, false)
+var jd10 = document.getElementsByClassName('zhongg')[0]
+var jd2 = document.getElementsByClassName('caidan')[0]
+var jd3 = document.getElementsByClassName('box')[0]
+var jd4 = document.getElementsByClassName('ce')[0]
+// console.log(jd2.innerHTML)
+jd2.addEventListener('click', function () {
+    console.log(123456)
+    // jd3.style.display = 'none'
+    jd10.style.display = ''
+    jd4.style.display = ''
+    document.documentElement.style.overflow = 'hidden';
+  
+}, false)
+
+var jd5 = document.getElementsByClassName('swww')[0]
+jd4.addEventListener('click', function () {
+    console.log(123456)
+    // jd3.style.display = 'none'
+    jd10.style.display = 'none'
+    jd4.style.display = 'none'
+    document.documentElement.style.overflow = 'scroll';
+}, false)
+
+
+  // if (jd1.children[2].style.display == 'none') {
+            //     console.log(1234556)
+            //     c.children[0].style.display = 'flex'
+            //     c.children[1].style.display = 'none'
+            // }else {
+            //     console.log("aaaaaaa")
+            //     c.children[0].style.display = 'flex'
+            //     c.children[1].style.display = 'none'
+            // }
