3f18f13179e6995821e4ad345cf377f2e5ed5699
Daisy Fang <nffa@student.unimelb.edu.au>
2022-05-23 16:37:43 +1000

    update js

---
+++ product listing 2/script.js
@@ -0,0 +1,6 @@
+var jd4 = document.getElementsByClassName('tz')[0]
+
+jd4.addEventListener('click', function () {
+  console.log(555555)
+  window.location.href="../homepage 1/index.html"
+}, false)
