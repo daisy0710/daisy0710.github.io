30a2acfe8de7bfd748a4161eb837ece4ee31fd33
Daisy Fang <nffa@student.unimelb.edu.au>
2022-05-31 15:53:14 +1000

    all 5 complete

---
+++ payment page5/script.js
@@ -0,0 +1,38 @@
+var jd1 = document.getElementsByClassName('first2') 
+var jd2 = document.getElementsByClassName('seco2')
+var jd3 = document.getElementsByClassName('fangg')
+var jdd = document.getElementsByClassName('bj')
+
+jdd[0].addEventListener('click', function () {
+    jd1[0].style.display = 'none'
+    jd1[1].style.display =""
+    jd1[2].style.display = 'none'
+    jd1[3].style.display =""
+    jd2[0].style.display = 'none'
+    jd2[1].style.display =""
+    jd2[2].style.display = 'none'
+    jd2[3].style.display =""
+    jd2[4].style.display = 'none'
+    jd2[5].style.display =""
+    jd2[7].style.display = 'none'
+    jd2[8].style.display =""
+    jd2[9].style.display = 'none'
+    jd2[10].style.display =""
+    jd2[11].style.display = 'none'
+    jd2[12].style.display =""
+    jd2[13].style.display = 'none'
+    jd2[14].style.display =""
+    jd3[0].style.display = 'none'
+    jd3[1].style.display =""
+}, false)
+var jd4 = document.getElementsByClassName('revv')[0]
+var jd5 = document.getElementsByClassName('rev')[0]
+jd4.addEventListener('click', function () {
+    console.log(555555)
+    window.location.href="../4/index.html"
+  }, false)
+  
+jd5.addEventListener('click', function () {
+    console.log(555555)
+    window.location.href="../6/index.html"
+  }, false)
